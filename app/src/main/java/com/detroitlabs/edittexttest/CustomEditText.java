0069eabbb5cfa17d01004e7467c764f4d4946da1
Andrew Giang <andrew.giang@detroitlabs.com>
2015-04-22 13:27:19 -0400

    Initial commit

---
+++ app/src/main/java/com/detroitlabs/edittexttest/CustomEditText.java
@@ -0,0 +1,25 @@
+package com.detroitlabs.edittexttest;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+/**
+ * Created by andrewgiang on 4/14/15.
+ */
+public class CustomEditText extends AppCompatEditText {
+
+    public CustomEditText(Context context) {
+        super(context);
+
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
